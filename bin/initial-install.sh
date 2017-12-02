d25832d541978339cae81a9644a00e87494bf9a0
Diko <diko316@gmail.com>
2017-12-02 20:28:16 +0800

    initial dev package install

---
+++ bin/initial-install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd $(dirname $(dirname $(readlink -f $0)))
+
+npm install --save-dev -dd -y "jasmine-core" \
+    "karma" \
+    "karma-firefox-launcher" \
+    "karma-jasmine" \
+    "karma-mocha-reporter" \
+    "karma-rollup-preprocessor" \
+    "rollup" \
+    "rollup-plugin-browsersync" \
+    "rollup-plugin-buble" \
+    "rollup-plugin-commonjs" \
+    "rollup-plugin-json" \
+    "rollup-plugin-node-builtins" \
+    "rollup-plugin-node-globals" \
+    "rollup-plugin-node-resolve" \
+    "rollup-plugin-uglify"
+
+
+rm -Rf node_modules
